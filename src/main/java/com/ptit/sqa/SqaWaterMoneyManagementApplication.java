8923f7cbc1a533365697abc0bdc66efdbbdc84e1
leonard <trinhvandat90399@gmail.com>
2021-03-24 22:31:05 +0700

    Create: initial project

---
+++ src/main/java/com/ptit/sqa/SqaWaterMoneyManagementApplication.java
@@ -0,0 +1,13 @@
+package com.ptit.sqa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqaWaterMoneyManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SqaWaterMoneyManagementApplication.class, args);
+    }
+
+}
