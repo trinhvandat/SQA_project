d32dc2a8e1bebdca25e355fa2c53673371932291
leonard <trinhvandat90399@gmail.com>
2021-03-27 12:02:34 +0700

    Update(CustomerInvoiceDTo): change Customer -> CustomerDTO

--- src/main/java/com/ptit/sqa/dto/response/CustomerInvoiceDTO.java
+++ src/main/java/com/ptit/sqa/dto/response/CustomerInvoiceDTO.java
@@ -1,15 +1,20 @@
 package com.ptit.sqa.dto.response;
 
-import com.ptit.sqa.model.Customer;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class CustomerInvoiceDTO {
     private int id;
     private Integer newWaterIndexUsed;
     private int oldWaterIndexUsed;
     private LocalDateTime timeUsing;
-    private Customer customer;
+    private CustomerDTO customer;
 }
