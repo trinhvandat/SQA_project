4bc16d241c0afa6a27c503d1577063f281d999ac
leonard <trinhvandat90399@gmail.com>
2021-05-27 00:40:32 +0700

    Fix: handler invalid exception for sending email, call back when email can't send

--- src/main/java/com/ptit/sqa/service/kafka/KafkaSubscriberIml.java
+++ src/main/java/com/ptit/sqa/service/kafka/KafkaSubscriberIml.java
@@ -1,10 +1,13 @@
 package com.ptit.sqa.service.kafka;
 
 import com.ptit.sqa.dto.response.CustomerInvoiceDTO;
+import com.ptit.sqa.repository.CustomerInvoiceRepository;
 import com.ptit.sqa.service.EmailService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.support.Acknowledgment;
+import org.springframework.messaging.handler.annotation.Payload;
 
 import java.util.concurrent.CountDownLatch;
 
@@ -14,18 +17,29 @@ import java.util.concurrent.CountDownLatch;
 public class KafkaSubscriberIml implements KafkaSubscriber{
 
     private final EmailService emailService;
+    private final CustomerInvoiceRepository customerInvoiceRepository;
 
     private CountDownLatch latch = new CountDownLatch(1);
     private String payload = null;
 
     @Override
-    public boolean HandlerEventSendEmailNotification(CustomerInvoiceDTO customerInvoice) {
+    public boolean HandlerEventSendEmailNotification(@Payload CustomerInvoiceDTO invoice, Acknowledgment ack) {
         log.info("receive");
-        boolean notificationResult = emailService.noticePaymentBill(customerInvoice);
-        log.info("Result send water bill notification to customer: {} is: {}", customerInvoice.getCustomer().getName(), notificationResult);
-        setPayload(customerInvoice.toString());
-        latch.countDown();
-        return notificationResult;
+        ack.acknowledge();
+        try {
+            boolean notificationResult = emailService.noticePaymentBill(invoice);
+            log.info("Result send water bill notification to customer: {} is: {}", invoice.getCustomer().getName(), notificationResult);
+            setPayload(invoice.toString());
+            latch.countDown();
+            return notificationResult;
+        } catch (Exception e){
+            customerInvoiceRepository.findById(invoice.getId())
+                    .ifPresent(customerInvoice -> {
+                        customerInvoice.setNewWaterIndexUsed(null);
+                        customerInvoiceRepository.save(customerInvoice);
+                    });
+            throw new RuntimeException("Send Email to customer fail");
+        }
     }
 
     @Override
